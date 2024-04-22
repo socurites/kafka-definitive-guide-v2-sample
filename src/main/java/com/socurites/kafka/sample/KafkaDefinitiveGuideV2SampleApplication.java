a0fe92b95690833c4b8c6784d6a29fc89ccbc113
socurites <socurites@gmail.com>
2024-04-22 20:06:47 +0900

    init

---
+++ src/main/java/com/socurites/kafka/sample/KafkaDefinitiveGuideV2SampleApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.kafka.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDefinitiveGuideV2SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDefinitiveGuideV2SampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socurites/kafka/sample/KafkaDefinitiveGuideV2SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socurites.kafka.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDefinitiveGuideV2SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
